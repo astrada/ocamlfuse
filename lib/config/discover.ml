e35e76bee3b06806256b5bfca108b7697267cd5c
Alessandro Strada <alessandro.strada@gmail.com>
2019-04-08 20:14:28 +0200

    Add ocamlfind failover for camlidl

--- lib/config/discover.ml
+++ lib/config/discover.ml
@@ -9,23 +9,33 @@ let () =
       let conf =
         match C.Pkg_config.get c with
         | Some pkgc -> (
-          match C.Pkg_config.query pkgc ~package:"fuse" with
-          | Some flags -> flags
-          | None -> default )
+            match C.Pkg_config.query pkgc ~package:"fuse" with
+            | Some flags -> flags
+            | None -> default )
         | None -> default
       in
       let calmidl_fname = "camlidl.libs.sexp" in
-      match
-        Sys.command
-          (Printf.sprintf "opam config var camlidl:lib > %s" calmidl_fname)
-      with
-      | 0 ->
-          let camlidl_lib_path =
-            String.trim (Io.String_path.read_file calmidl_fname)
-          in
-          let camlidl_libs = ["-L" ^ camlidl_lib_path; "-lcamlidl"] in
-          C.Flags.(
-            write_sexp calmidl_fname camlidl_libs ;
-            write_sexp "fuse.cflags.sexp" conf.cflags ;
-            write_sexp "fuse.libs.sexp" conf.libs)
-      | _ -> C.die "Could not query camlidl lib path" )
+      let camlidl_lib_path =
+        match
+          Sys.command
+            (Printf.sprintf "opam config var camlidl:lib > %s" calmidl_fname)
+        with
+        | 0 ->
+          String.trim (Io.String_path.read_file calmidl_fname)
+        | _ ->
+          begin match
+              Sys.command
+                (Printf.sprintf "ocamlfind query camlidl > %s" calmidl_fname)
+            with
+            | 0 ->
+              String.trim (Io.String_path.read_file calmidl_fname)
+            | _ ->
+              C.die "Could not query camlidl lib path"
+          end
+      in
+      let camlidl_libs = ["-L" ^ camlidl_lib_path; "-lcamlidl"] in
+      C.Flags.(
+        write_sexp calmidl_fname camlidl_libs ;
+        write_sexp "fuse.cflags.sexp" conf.cflags ;
+        write_sexp "fuse.libs.sexp" conf.libs)
+    )
