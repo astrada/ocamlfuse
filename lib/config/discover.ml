da78a8949e662540078540475b27fa8942a58939
Alessandro Strada <alessandro.strada@gmail.com>
2020-06-24 21:48:47 +0200

    Fix #14
    Remove dependency from non-public dune API

--- lib/config/discover.ml
+++ lib/config/discover.ml
@@ -1,5 +1,21 @@
-open! Stdune
 module C = Configurator.V1
+
+let read_file path =
+  let in_ch = open_in path in
+  let rec loop acc =
+    try
+      let line = input_line in_ch in
+      loop (acc ^ line)
+    with End_of_file -> acc
+  in
+  try
+    let result = loop "" in
+    close_in in_ch;
+    result
+  with exc ->
+    close_in in_ch;
+    Printf.eprintf "read_file: could not read file: '%s'" path;
+    raise exc
 
 let () =
   C.main ~name:"foo" (fun c ->
@@ -20,13 +36,13 @@ let () =
           Sys.command
             (Printf.sprintf "opam config var camlidl:lib > %s" calmidl_fname)
         with
-        | 0 -> String.trim (Io.String_path.read_file calmidl_fname)
+        | 0 -> String.trim (read_file calmidl_fname)
         | _ -> (
             match
               Sys.command
                 (Printf.sprintf "ocamlfind query camlidl > %s" calmidl_fname)
             with
-            | 0 -> String.trim (Io.String_path.read_file calmidl_fname)
+            | 0 -> String.trim (read_file calmidl_fname)
             | _ -> C.die "Could not query camlidl lib path" )
       in
       let camlidl_libs = [ "-L" ^ camlidl_lib_path; "-lcamlidl" ] in
